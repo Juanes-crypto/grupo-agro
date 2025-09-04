37df4f4d76e685d28600425deae3320ccbb86687
juanes <juangarciaa@ieatanasio.edu.co>
2025-09-04 17:39:32 -0500

    he cambiado el valor de baseURL quitando el sufijo /api

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Configuración automática para desarrollo/producción - CORREGIDO para Vite
 const baseURL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api";
+  import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
 
 console.log("Axios Base URL:", baseURL);
 
